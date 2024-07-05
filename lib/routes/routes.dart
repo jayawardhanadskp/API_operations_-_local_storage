1c36c3dba087d512c8e31cf854c0b9c1418c8b61
kasun <kasunpradeep.ds@gmail.com>
2024-07-05 10:17:04 +0530

    routes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,7 @@
+import 'package:api_local_storage/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  HomeScreen.routeName: (_) => const HomeScreen(),
+
+};
